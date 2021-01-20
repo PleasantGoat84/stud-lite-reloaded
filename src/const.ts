0e10531b9ea583a0b015b1ae1d1702dbbfea8f6d
PleasantGoat84 <PleasantGoat84@gmail.com>
2021-01-20 19:34:43 +0800

    as backend updates

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
 export const apiRoot = "http://localhost:8484/lh-api/";
-export const SSOUrl = `${apiRoot}sign-on?app=sl`;
+export const SSOUrl = `${apiRoot}link?app=sl`;
 export const ssoHandler = "http://localhost:3000/sso";
